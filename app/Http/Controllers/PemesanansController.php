d7971a03c24187b3a3341e497826fdc760f71a66
dellavnt <112696211+dellavnt@users.noreply.github.com>
2023-02-06 15:05:37 +0700

    perbaikan nama controller dan model

--- app/Http/Controllers/PemesanansController.php
+++ app/Http/Controllers/PemesanansController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class PemesananController extends Controller
+class PemesanansController extends Controller
 {
     /**
      * Display a listing of the resource.
